3ae335c22c96cb73e789dc7f260b62ce8a42adcf
Wisnu Y <100105501+wysts@users.noreply.github.com>
2022-09-08 16:26:27 +0700

    praktikum

---
+++ praktikum2/src/bangundatar/BangunDatar.java
@@ -0,0 +1,15 @@
+package bangundatar;
+
+public class BangunDatar {
+	protected float luas, keliling;
+	
+	public void hitungLuas() {}
+	public void hitungKeliling() {}
+	
+	public void printInfo() {
+		System.out.println("Luas\t: \t" + luas);
+		System.out.println("Keliling: \t" + keliling);
+	  
+	}
+	  
+}
---
+++ praktikum2/src/bangunruang/Balok.java
@@ -0,0 +1,28 @@
+package bangunruang;
+
+public class Balok extends BangunRuang {
+	private float p, l, t;
+
+	public Balok(float _p, float _l, float _t) {
+		this.p = _p;
+    	this.l = _l;
+    	this.t = _t;
+  
+	}
+
+	public void hitungLuas() {
+		this.luas = 2 * ((p * l) + (l * t) + (p * t));
+	
+	}
+
+	public void hitungKeliling() {
+		this.keliling = 4 * (p + l + t);
+	
+	}
+
+	public void hitungVolume() {
+		this.volume = p * l * t;
+  
+	}
+	
+}
